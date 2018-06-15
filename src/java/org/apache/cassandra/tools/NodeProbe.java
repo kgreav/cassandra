a331bb519515a7ba712014399f9c919d529324ba
jaydeepkumar1984 <chovatia.jaydeep@gmail.com>
2018-06-14 21:43:32 -0700

    Don't enable native transport if bootstrap is pending.

--- src/java/org/apache/cassandra/tools/NodeProbe.java
+++ src/java/org/apache/cassandra/tools/NodeProbe.java
@@ -623,6 +623,8 @@ public class NodeProbe implements AutoCloseable
         return ssProxy.isDraining();
     }
 
+    public boolean isBootstrapMode() { return ssProxy.isBootstrapMode(); }
+
     public void joinRing() throws IOException
     {
         ssProxy.joinRing();
